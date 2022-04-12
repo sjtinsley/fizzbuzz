de8d8cde10cdb4c51ca727c13c52535921945f6a
sjtinsley <simonjtinsley@gmail.com>
2022-04-12 15:23:52 +0100

    Wrote tests for 1 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,10 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns 1 when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
 end
