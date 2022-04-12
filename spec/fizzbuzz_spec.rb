93fae8bf5b73682ccbd9923c4052039bddf80d6f
Erlantz <erlantzram@gmail.com>
2022-04-12 15:29:36 +0100

    added 2 more tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,4 +9,10 @@ describe 'fizzbuzz' do
   it 'returns 1 when passed 1' do
     expect(fizzbuzz(1)).to eq 1
   end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'return 7 when passed 7' do
+    expect(fizzbuzz(7)).to eq 7
+  end
 end
